f11cd8b74be5ea536fd23ea6ecf40cdfdf5af321
ParkSenn <102174849+ParkSenn@users.noreply.github.com>
2023-12-05 13:40:17 +0900

    feat: 사진 업로드 구현

--- src/main/java/controller/product/ProductController.java
+++ src/main/java/controller/product/ProductController.java
@@ -4,17 +4,36 @@ package controller.product;
 import model.Product;
 import model.service.ProductManager;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
+import java.util.UUID;
+
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import controller.Controller;
-import controller.comm.CreateCommunityController;
 
+
+
+@WebServlet("/ProductController")
+@MultipartConfig(
+    fileSizeThreshold = 1024 * 1024 * 2,  // 2MB
+    maxFileSize = 1024 * 1024 * 10,       // 10MB
+    maxRequestSize = 1024 * 1024 * 50     // 50MB
+)
 public class ProductController implements Controller {
     private static final Logger log = LoggerFactory.getLogger(ProductController.class);
     
@@ -22,38 +41,11 @@ public class ProductController implements Controller {
     public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
         request.setCharacterEncoding("utf-8");
      // 'title' 파라미터 값 로깅
-        String title = request.getParameter("title");
+        String title = getValue(request.getPart("title"));
         log.debug("Title parameter: {}", title); // SLF4J 로깅
-
-        // System.out.println을 사용하여 콘솔에 출력할 수도 있습니다.
         System.out.println("Title parameter: " + title);
-//        Product product = new Product(
-//                0, 
-//                Integer.parseInt(request.getParameter("regular_price")),
-//                Integer.parseInt(request.getParameter("rental_fee")),
-//                request.getParameter("description"),
-//                Integer.parseInt(request.getParameter("deposit")),
-//                request.getParameter("product_photo"),
-//                request.getParameter("address"),
-//                request.getParameter("detail_address"),
-//                false,
-//                Integer.parseInt(request.getParameter("customerId")),
-//                request.getParameter("title"));  
-//        try {
-//            ProductManager productManager = ProductManager.getInstance();
-//            productManager.addProduct(product);
-//
-//            log.debug("Create Product : {}", product);
-//            return "redirect:/product/view"; // 성공시 물 리스트 화면으로 redirect
-//            
-//        } catch (Exception e) {     // 예외 발생 시 입력 form으로 forwarding
-//            request.setAttribute("creationFailed", true);
-//            request.setAttribute("exception", e);
-//            request.setAttribute("product", product);
-//            return "/product/addProductForm.jsp"; 
-//        }
                 
-        String action = request.getParameter("action");
+        String action = getValue(request.getPart("action"));
 //        if (action == null || action.isEmpty()) {
 //            return "/product/addProductForm.jsp"; // GET 요청에 대해 addProductForm.jsp 반환
 //        }
@@ -75,19 +67,49 @@ public class ProductController implements Controller {
     
     // 상품 등록
     private String addProduct(HttpServletRequest request, HttpServletResponse response)  throws Exception {
+        Part filePart = request.getPart("product_photo");
+        String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString();
+        // UUID 생성
+        String uniqueID = UUID.randomUUID().toString();
+        String newFileName = uniqueID + "_" + fileName;
+        // 업로드 폴더의 실제 서버 경로를 찾음
+        String uploadPath = request.getServletContext().getRealPath("") + File.separator + "upload";
+        File uploadDir = new File(uploadPath);
+        if (!uploadDir.exists()) uploadDir.mkdir();
+        String saveAs = uploadPath + File.separator + newFileName;
+
+        // 파일 저장
+        try (InputStream fileContent = filePart.getInputStream();
+             FileOutputStream fos = new FileOutputStream(saveAs)) {
+            int read;
+            final byte[] bytes = new byte[1024];
+            while ((read = fileContent.read(bytes)) != -1) {
+                fos.write(bytes, 0, read);
+            }
+        }  
+        
+        String title = getValue(request.getPart("title"));
+        String description = getValue(request.getPart("description"));
+        int regularPrice = Integer.parseInt(getValue(request.getPart("regular_price")));
+        int rentalFee = Integer.parseInt(getValue(request.getPart("rental_fee")));
+        int deposit = Integer.parseInt(getValue(request.getPart("deposit")));
+        String category = getValue(request.getPart("category"));
+        String address = getValue(request.getPart("address"));
+        String detailAddress = getValue(request.getPart("detail_address"));
+        int customerId = Integer.parseInt(getValue(request.getPart("customerId")));
+             
         Product product = new Product(
                 0, 
-                Integer.parseInt(request.getParameter("regular_price")),
-                Integer.parseInt(request.getParameter("rental_fee")),
-                request.getParameter("description"),
-                Integer.parseInt(request.getParameter("deposit")),
-                
-                request.getParameter("product_photo"),
-                request.getParameter("address"),
-                request.getParameter("detail_address"),
+                regularPrice,
+                rentalFee,
+                description,
+                deposit,                
+                newFileName,
+                address,
+                detailAddress,
                 false,
-                Integer.parseInt(request.getParameter("customerId")),
-                request.getParameter("title"));  
+                customerId,
+                title);
         try {
             ProductManager productManager = ProductManager.getInstance();
             productManager.addProduct(product);
@@ -100,6 +122,20 @@ public class ProductController implements Controller {
             request.setAttribute("exception", e);
             request.setAttribute("product", product);
             return "/product/addProductForm.jsp"; 
+        }
+    }
+    
+ // Part에서 값을 추출하는 메소드
+    private String getValue(Part part) throws IOException {
+        try (InputStream inputStream = part.getInputStream();
+             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+            StringBuilder value = new StringBuilder();
+            char[] buffer = new char[1024];
+            int length;
+            while ((length = reader.read(buffer)) != -1) {
+                value.append(buffer, 0, length);
+            }
+            return value.toString();
         }
     }
 
--- src/main/webapp/WEB-INF/product/addProductForm.jsp
+++ src/main/webapp/WEB-INF/product/addProductForm.jsp
@@ -7,9 +7,9 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <title>빌려중고?</title>
 <link rel="stylesheet" type="text/css"
-	href="<c:url value='/css/global.css'/>">
+    href="<c:url value='/css/global.css'/>">
 <link rel="stylesheet" type="text/css"
-	href="<c:url value='/css/addProductForm.css'/>">
+    href="<c:url value='/css/addProductForm.css'/>">
 <script>
 function productCreate() {
     if (myform.title.value == "") {
@@ -23,54 +23,54 @@ function productCreate() {
 </script>
 </head>
 <body>
-	<div class="header">
-		<img class="back-icon"
-			src="<c:url value='/images/addProductForm/Vector.svg'/>" />
-		<div class="header-text">물품 등록 하기</div>
-		<img class="cart-icon"
-			src="<c:url value='/images/addProductForm/simple-line-icons_basket.svg'/>" />
-	</div>
+    <div class="header">
+        <img class="back-icon"
+            src="<c:url value='/images/addProductForm/Vector.svg'/>" />
+        <div class="header-text">물품 등록 하기</div>
+        <img class="cart-icon"
+            src="<c:url value='/images/addProductForm/simple-line-icons_basket.svg'/>" />
+    </div>
 
-	<!-- 물건 등록 form -->
-	<form name="myform" method="POST" class="add-form"
-		action="<c:url value='/product/create' />">
-		<div class="input">
-<!-- 			<div class="input-title">물품 사진</div>
-			<input type="file" class="input-text" name="product_photo"><br>
-			<br> -->
-			<div class="input-title">물품 이름</div>
-			<input class="input-text" placeholder="물품 이름을 입력해주세요." type="text"
-				name="title" />
-			<div class="input-title">물품 설명</div>
-			<input class="input-text" placeholder="물품 설명을 입력해주세요." type="text"
-				name="description" />
+    <!-- 물건 등록 form -->
+    <form name="myform" method="POST" class="add-form" enctype="multipart/form-data"
+        action="<c:url value='/product/create' />">
+        <div class="input">
+            <div class="input-title">물품 사진</div>
+            <input type="file" class="input-text" name="product_photo"><br>
+            <br>
+            <div class="input-title">물품 이름</div>
+            <input class="input-text" placeholder="물품 이름을 입력해주세요." type="text"
+                name="title" />
+            <div class="input-title">물품 설명</div>
+            <input class="input-text" placeholder="물품 설명을 입력해주세요." type="text"
+                name="description" />
 
-			<div class="input-title">물품의 정가</div>
-			<input class="input-text" placeholder="물품의 정가를 입력해주세요." type="text"
-				name="regular_price" />
-			<div class="input-title">대여비</div>
-			<input class="input-text" placeholder="물품의 대여비를 입력해주세요." type="text"
-				name="rental_fee" />
-			<div class="input-title">보증금</div>
-			<input class="input-text" placeholder="물품의 보증금을 입력해주세요." type="text"
-				name="deposit" />
-			<div class="input-title">물품 카테고리</div>
-			<input class="input-text" placeholder="물품 카테고리를 입력해주세요." type="text"
-				name="category" />
-			<div class="input-title">대여 진행 장소</div>
-			<input class="input-text" placeholder="대여를 진행할 장소를 입력해주세요."
-				type="text" name="address" />
-			<div class="input-title">대여 상세 주소</div>
-			<input class="input-text" placeholder="대여를 진행할 상세 주소를 입력해주세요."
-				type="text" name="detail_address" />
-		</div>
-		<input type="hidden" name="action" value="add" />
-		<!-- 로그인 구현 전까지 임시 customerId -->
-		<input type="hidden" name="customerId" value="1" />
-		<input type="hidden" name="product_photo" value="photo1234.jpg" />
-		<!-- <input type="button" value="물건 등록" onClick="productCreate()" /> -->
-		<button type="submit" class="select-date-btn" onClick="productCreate()">물건 등록</button>
-	</form>
+            <div class="input-title">물품의 정가</div>
+            <input class="input-text" placeholder="물품의 정가를 입력해주세요." type="text"
+                name="regular_price" />
+            <div class="input-title">대여비</div>
+            <input class="input-text" placeholder="물품의 대여비를 입력해주세요." type="text"
+                name="rental_fee" />
+            <div class="input-title">보증금</div>
+            <input class="input-text" placeholder="물품의 보증금을 입력해주세요." type="text"
+                name="deposit" />
+            <div class="input-title">물품 카테고리</div>
+            <input class="input-text" placeholder="물품 카테고리를 입력해주세요." type="text"
+                name="category" />
+            <div class="input-title">대여 진행 장소</div>
+            <input class="input-text" placeholder="대여를 진행할 장소를 입력해주세요."
+                type="text" name="address" />
+            <div class="input-title">대여 상세 주소</div>
+            <input class="input-text" placeholder="대여를 진행할 상세 주소를 입력해주세요."
+                type="text" name="detail_address" />
+        </div>
+        <input type="hidden" name="action" value="add" />
+        <!-- <!— 로그인 구현 전까지 임시 customerId —> -->
+        <input type="hidden" name="customerId" value="1" />
+        <input type="hidden" name="product_photo" value="photo1234.jpg" />
+        
+        <button type="submit" class="select-date-btn" onClick="productCreate()">물건 등록</button>
+    </form>
 
 
 </body>
