934a3bf268a0f4c06396fd4524410a5ec382db90
Yeji <mnbvcxzyj@gmail.com>
2023-12-07 00:42:28 +0900

    [Fix] 리스트 버튼 추가, 수정중

--- src/main/java/controller/user/LoginController.java
+++ src/main/java/controller/user/LoginController.java
@@ -4,16 +4,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import controller.Controller;
+import controller.DispatcherServlet;
 import model.User;
 import model.service.UserManager;
 
 public class LoginController implements Controller {
+    private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
+
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
         String email = request.getParameter("email");
         String passwd = request.getParameter("password");
-        
+        logger.debug(email+passwd);
         try {
             // 모델에 로그인 처리를 위임
             UserManager manager = UserManager.getInstance();
@@ -24,7 +30,10 @@ public class LoginController implements Controller {
             HttpSession session = request.getSession();
             session.setAttribute(UserSessionUtils.USER_SESSION_KEY, email);
             session.setAttribute("customerId", user.getCustomerId());
+            session.setAttribute("name",  user.getName());
             
+            logger.debug(Integer.toString(user.getCustomerId()));
+            logger.debug(user.getName());
             return "redirect:/product/list";        
         } catch (Exception e) {
             /* UserNotFoundException이나 PasswordMismatchException 발생 시
